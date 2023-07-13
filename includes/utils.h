20b7cd993fbc65d96318de031fb8c104e4e7d805
Baptiste Bessard <bat@wl4s-84-168.unil.ch>
2023-07-13 18:13:56 +0200

    syntax

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: bat <bat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 17:16:00 by bat               #+#    #+#             */
-/*   Updated: 2023/07/13 18:08:45 by bat              ###   ########.fr       */
+/*   Updated: 2023/07/13 18:13:44 by bat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ void	set_state(t_philo *philos, int n, t_state state);
 t_state	get_state(t_philo *philos, int n);
 void	ft_wait_ms(int n);
 long long	get_time_ms(void);
-void	ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
